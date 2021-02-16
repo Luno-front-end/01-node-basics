3d19f37a95cbc8d081245ad6db83df31a04a31fb
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-02-16 21:11:37 +0200

    add full fn

---
+++ contacts.js
@@ -0,0 +1,64 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join("./db/contacts.json");
+
+// fs.writeFile(filename, data, [options]);
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => console.table(data.toString()))
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// console.table(listContacts());
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) =>
+      console.table(JSON.parse(data).find(({ id }) => id === contactId))
+    )
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// console.table(getContactById());
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+      const deleteContact = parsedData.filter(({ id }) => id !== contactId);
+
+      console.table(deleteContact);
+      fs.writeFile(contactsPath, JSON.stringify(deleteContact));
+      return deleteContact;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+// removeContact(10);
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const parsedData = JSON.parse(data);
+      const newContact = { name, email, phone };
+      const addContact = parsedData.push(newContact);
+
+      console.table(addContact);
+      fs.writeFile(contactsPath, JSON.stringify(parsedData));
+      return addContact;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// addContact("max", "gma@gma", 2312852);
+
+module.export = { listContacts, getContactById, removeContact, addContact };
